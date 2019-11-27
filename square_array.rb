7222a0aee24130cb3662f4d8026638211dfd6283
Michael Fahim <michael_fahim@yahoo.com>
2019-11-27 02:05:11 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   # your code here
-  array.each do |i|
-  new_array = i**2
+  new_array = []
 
+   array.each do |integer|
+    new_array << integer ** 2
+   end
+   return new_array
   end
-
-end
